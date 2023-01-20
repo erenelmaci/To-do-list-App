29508e2d91dbdd86bef516b41216866cf80fe7d2
Eren Elmacı <erenx1623@gmail.com>
2023-01-20 17:29:49 +0300

    update

--- app.js
+++ app.js
@@ -1,67 +1,93 @@
+const todoTitle = document.getElementById("todo-input");
+const todoAbout = document.getElementById("todo-about");
 const addButton = document.querySelector(".add");
-const titleInput = document.getElementsByClassName("title")[0];
-const aboutInput = document.getElementsByClassName("about")[0];
-const main = document.querySelector("main");
-const deleteButton = document.querySelector(".delete");
-const sectionList = document.querySelector(".section");
-const editButton = document.querySelector(".done");
-const checkbox = document.querySelector("input[type='checkbox']");
-const savedCheckbox = localStorage.getItem("checkbox");
+const todoUl = document.querySelector(".todo-ul");
 
-addButton.addEventListener("click", () => {
-  add();
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
+window.addEventListener("load", () => {
+  getTodoListFromLocalStorage();
 });
 
-function add() {
-  sectionList.innerHTML += `<section>
-   <div class="todo">
-  <div class="todo-list">
-    <div class="todo-title">
-      <h2>${titleInput.value}</h2>
-    </div>
-    <div class="todo-about">
-      <p>
-        ${aboutInput.value}
-      </p>
-    </div>
-  </div>
-  <div class="todo-buttons">
-    <button class="done bstyle ">
-      <i class="fa-solid fa-pen-to-square done"></i>
-    </button>
-    <input class="checkbox bstyle" type="checkbox" name="" id="" />
-    <button class="delete bstyle">
-      <i class="fa-regular fa-trash-can delete"></i>
-    </button>
-  </div>
-</div>
-</section>`;
-  titleInput.value = "";
-  aboutInput.value = "";
-  saveToLocalStorage();
-}
+const getTodoListFromLocalStorage = () => {
+  todoList.forEach((todo) => {
+    createTodo(todo);
+  });
+};
 
-sectionList.addEventListener("click", (event) => {
-  if (event.target.classList.contains("delete")) {
-    event.target.parentElement.parentElement.parentElement.remove();
-  } else if (event.target.classList.contains("done")) {
-    let title = event.target.parentElement.parentElement.children[0].children[0].children[0];
-    let about = event.target.parentElement.parentElement.children[0].children[1].children[0];
-
-    title.setAttribute("contentEditable", true);
-    about.setAttribute("contentEditable", true);
-  }else if (savedCheckbox === "checked") {
-    // const savedCheckbox = localStorage.getItem("checkbox");
-    checkbox.checked = true;
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (todoTitle.value.trim() === "" || todoAbout.value.trim() === "") {
+    alert("Please, enter new todo text");
+    return;
   }
-  saveToLocalStorage();
+  const newTodo = {
+    id: new Date().getTime(),
+    completed: false,
+    title: todoTitle.value,
+    about: todoAbout.value,
+  };
+
+  createTodo(newTodo);
+  console.log(newTodo);
+
+  todoList.push(newTodo);
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  e.target.closest("form").reset();
 });
 
-function saveToLocalStorage() {
-  localStorage.setItem("todo-list", sectionList.innerHTML);
-}
+const createTodo = (newTodo) => {
+  const { id, completed, title, about } = newTodo;
 
-const savedList = localStorage.getItem("todo-list");
-if (savedList) {
-  sectionList.innerHTML = savedList;
-}
+  const li = document.createElement("li");
+  li.setAttribute("id", id);
+
+  completed ? li.classList.add("checked") : "";
+  li.setAttribute("id", id);
+
+  const icon = document.createElement("i");
+  icon.setAttribute("class", "fas fa-check");
+  li.append(icon);
+
+  const pContainer = document.createElement("div");
+  pContainer.classList.add("pContainer");
+  li.appendChild(pContainer);
+
+  const pTitleText = document.createElement("p");
+  pTitleText.innerText = title;
+  pTitleText.classList.add("pTitleText");
+  pContainer.appendChild(pTitleText);
+
+  const pAboutText = document.createElement("p");
+  pAboutText.innerText = about;
+  pContainer.appendChild(pAboutText);
+
+  const removeIcon = document.createElement("i");
+  removeIcon.setAttribute("class", "fas fa-trash");
+
+  li.append(removeIcon);
+
+  todoUl.prepend(li);
+};
+
+todoUl.addEventListener("click", (e) => {
+  const idAttr = e.target.closest("li").getAttribute("id");
+
+  if (e.target.classList.contains("fa-check")) {
+    e.target.parentElement.classList.toggle("checked");
+    todoList.forEach((todo) => {
+      if (todo.id == idAttr) {
+        todo.completed = !todo.completed;
+      }
+    });
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  } else if (e.target.classList.contains("fa-trash")) {
+    e.target.parentElement.remove();
+    todoList = todoList.filter((todo) => todo.id != idAttr);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  } else {
+    alert("other element clicked");
+  }
+  console.log(todoList);
+});
